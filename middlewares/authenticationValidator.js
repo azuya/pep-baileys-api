3f6c13c6268b48cbb92d5907e82709b11a6f1ca7
Disketsa - Azuya <azuya@users.noreply.github.com>
2024-05-13 19:51:13 +0700

    Update authenticationValidator.js
    add Authentication method from Header Request

--- middlewares/authenticationValidator.js
+++ middlewares/authenticationValidator.js
@@ -1,6 +1,13 @@
 import response from './../response.js'
 
 const validate = (req, res, next) => {
+    const apiKeyHeader = process.env.AUTHENTICATION_GLOBAL_AUTH_TOKEN;
+    if (apiKeyHeader === undefined) return next();
+    const reqHeaderApiKey = req.headers["x-api-key"] || req.headers["X-API-Key"] || req.headers["apikey"];
+    if (reqHeaderApiKey === apiKeyHeader) {
+        return next();
+    }
+    
     const apiKey = req.get('apikey') ?? req.query.apikey
 
     if (!process.env.AUTHENTICATION_GLOBAL_AUTH_TOKEN) {
