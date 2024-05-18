b0233cc0903bf5b0e65518d58dac92dabd8ecb58
Disketsa - Azuya <azuya@users.noreply.github.com>
2024-05-18 23:44:01 +0700

    Update contactsRoute.js

--- routes/contactsRoute.js
+++ routes/contactsRoute.js
@@ -1,1 +1,13 @@
 
+
+import { Router } from 'express'
+import { body, query } from 'express-validator'
+import requestValidator from '../middlewares/requestValidator.js'
+import sessionValidator from '../middlewares/sessionValidator.js'
+import * as controller from '../controllers/contactsController.js'
+
+const router = Router()
+
+router.get("/:jid", query('id').notEmpty(), requestValidator, sessionValidator, controller.check);
+
+export default router
