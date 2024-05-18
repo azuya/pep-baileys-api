0ac7a9996e436776cbcd076324aaa25975f4d8c6
azuya <disketsa@gmail.com>
2024-05-19 01:43:55 +0700

    Changes to be committed: 	modified:   controllers/contactsController.js

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -4,14 +4,14 @@ import response from './../response.js'
 const check = async (req, res) => {
     try {
         const session = getSession(res.locals.sessionId)
-        const { jid } = req.params
+		const { jid } = req.params
         const exists = await isExists(session, jid)
         if (!exists) {
             return response(res, 200, false, 'The receiver number is not exists.')
         }
         response(res, 200, true, 'The receiver number is exists')
     } catch (e) {
-        const message = 'An error occured during jid check. NO JID ' + jid
+        const message = 'An error occured during jid check. NO JID '+jid
 
         response(res, 400, false, message)
         // res.status(500).json({ error: message })
