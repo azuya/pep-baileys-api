fd98542513c3f6e181f40a87cbc473d692b6fb3d
azuya <disketsa@gmail.com>
2024-05-19 01:06:21 +0700

    Changes to be committed: 	modified:   controllers/contactsController.js

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -1,18 +1,17 @@
-import { getSession, numberExist } from './../whatsapp.js'
+import { getSession, isExists } from './../whatsapp.js'
 import response from './../response.js'
 
 const check = async (req, res) => {
     try {
         const session = getSession(res.locals.sessionId)
-        const { message } = req.body
-
-        const exists = await numberExist(session, jid)
+        const { jid } = req.params
+        const exists = await isExists(session, jid)
         if (!exists) {
             return response(res, 200, false, 'The receiver number is not exists.')
         }
         response(res, 200, true, 'The receiver number is exists')
     } catch (e) {
-        const message = 'An error occured during jid check. NO JID'
+        const message = 'An error occured during jid check. NO JID ' + jid
 
         response(res, 400, false, message)
         // res.status(500).json({ error: message })
